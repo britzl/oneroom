948e7cdc47c2557bb6354958dfc12c8abf97ca8b
Björn Ritzl <bjorn.ritzl@king.com>
2016-12-29 11:19:06 +0100

    Added destructible terrain and screen shake

--- oneroom/bullet.go
+++ oneroom/bullet.go
@@ -13,6 +13,21 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "smoketrail"
+  component: "/oneroom/smoketrail.particlefx"
+  position {
+    x: 0.0
+    y: -4.389867
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "enemycollisionobject"
   type: "collisionobject"
